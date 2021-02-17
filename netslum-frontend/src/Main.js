fb01f11d8e109a1e917120fb30c7b6db21d8c4d8
ariel <ariel.suil@ug.uchile.cl>
2021-02-17 14:16:16 -0300

    fix: basic project structure

---
+++ netslum-frontend/src/Main.js
@@ -0,0 +1,13 @@
+import Header from "./sections/Header";
+import Body from "./sections/Body";
+import Footer from "./sections/Footer";
+
+const Main = () => (
+  <div>
+    <Header />
+    <Body />
+    <Footer />
+  </div>
+);
+
+export default Main;
---
+++ netslum-frontend/src/sections/Header.js
@@ -0,0 +1,5 @@
+const Header = () => (
+  <div>hola mundo</div>
+);
+
+export default Header;
