492caa6a9794f0de144b8bd844d1b09a03ec248c
ariel <ariel.suil@ug.uchile.cl>
2021-02-23 09:51:24 -0300

    fix: improve props management in Layer & LayerWrap

--- netslum-frontend/src/wrappers/Layer.js
+++ netslum-frontend/src/wrappers/Layer.js
@@ -1,26 +1,18 @@
 import Div from "./Div";
 
 const Layer = (props) => {
-  const {
-    top,
-    bottom,
-    right,
-    left,
-    children,
-  } = props;
   return (
     <Div
       {...props}
       style={{
-        top: bottom ? 'auto' : 0,
-        bottom: top ? 'auto' : 0,
-        right: left ? 'auto' : 0,
-        left: right ? 'auto' : 0,
+        ...props.style,
+        top: props.top ? 0 : 'auto',
+        bottom: props.bottom ? 0 : 'auto',
+        right: props.left ? 'auto' : 0,
+        left: props.right ? 'auto' : 0,
         position: 'absolute',
       }}
-    >
-      {children}
-    </Div>
+    />
   );
 };
 
--- netslum-frontend/src/wrappers/LayerWrap.js
+++ netslum-frontend/src/wrappers/LayerWrap.js
@@ -1,14 +1,14 @@
 import Div from "./Div";
 
 const LayerWrap = (props) => {
-  const { children } = props;
   return (
     <Div
       {...props}
-      style={{position: 'relative'}}
-    >
-      {children}
-    </Div>
+      style={{
+        ...props.style,
+        position: 'relative'
+      }}
+    />
   );
 };
 
