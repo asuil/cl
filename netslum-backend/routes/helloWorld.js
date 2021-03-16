1c1cff6f8cd5be47aa2df7b8f74ce95f76b6516e
ariel <ariel.suil@ug.uchile.cl>
2021-03-16 17:36:49 -0300

    feat: example for receiving POST and GET data

--- netslum-backend/routes/helloWorld.js
+++ netslum-backend/routes/helloWorld.js
@@ -3,10 +3,13 @@ const db = require('../utils/DB');
 
 const router = express.Router();
 router.get('/', (req, res) => {
+  /*
   db({
     onSuccess: () => { res.send('Conectado!'); },
     onFailure: (err) => { res.send(`:(\n${err}`); },
   });
+  */
+  res.send(`Body: ${JSON.stringify(req.body)}\nQuery: ${JSON.stringify(req.query)}`);
 });
 
 module.exports = router;
